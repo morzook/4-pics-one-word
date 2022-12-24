40bbb799719bf65c54684a58f94c83bbb00bf991
morzook <59170230+morzook@users.noreply.github.com>
2022-12-24 14:08:21 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,293 @@
+const game = [
+  {
+          imageBox1: `https://gazettengr.com/wp-content/uploads/Screenshot_20211009-093427_Chrome.jpg`,
+          imageBox2: 'https://guardian.ng/wp-content/uploads/2022/03/Screenshot-2022-03-14-at-19.12.18.png',
+          imageBox3: `https://upload.wikimedia.org/wikipedia/commons/8/81/CrouchingEyo.jpg`,
+          imageBox4: `https://businessday.ng/wp-content/uploads/2017/03/Lagos-Danfo.jpg`,
+          ansDiv: '',
+          randomAlphabets: 'ASDGERLOPINW',
+          alphabetDiv: '',
+          answer: 'LAGOS',
+  },
+  {
+    imageBox1: `https://miro.medium.com/max/664/1*rtW3PvtqJ86c5w9j1Nr88g.jpeg`,
+    imageBox2: `https://asoebiguest.com/wp-content/uploads/2021/07/asoebiguest_fce7c82922add87cf6fcf1030bdca85d-472x600.jpg`,
+    imageBox3: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5t0oeso4mD6-zzm-hwGM5Pg1kAGn9lRDzLpag5Fo9zdBoGJAEXrpzf_f414Sopggp7mc&usqp=CAU`,
+    imageBox4: `https://9jafoods.com/wp-content/uploads/2020/04/Nkwobi-food.jpg`,
+    ansDiv: '',
+    randomAlphabets: 'UBDEFGHIJKOZ',
+    alphabetDiv: '',
+    answer: 'IGBO',
+  },
+   {
+     imageBox1: `https://i0.wp.com/insidebusiness.ng/wp-content/uploads/2019/11/republic-of-amala-and-ewedu-with-stew-and-orisirisi-by-@@spiceplatter-.-.-.-.-.-africanfood-food-nai.jpg?fit=640%2C640&ssl=1`,
+     imageBox2: `https://9jafoods.com/wp-content/uploads/2020/04/Nkwobi-food.jpg`,
+     imageBox3: `https://www.myactivekitchen.com/wp-content/uploads/2019/05/nigerian-puff-puff-recipe-how-to-make-puff-puff-img-1.jpg`,
+     imageBox4: `https://lyndishes.com/wp-content/uploads/2020/10/101946261_2701421523516242_3742521552105402257_n.jpg`,
+     ansDiv: '',
+     randomAlphabets: 'PDYGEOMALFZO',
+    alphabetDiv: '',
+     answer: 'FOOD',
+    },
+    {
+      imageBox2: `https://media.allure.com/photos/5afee67cff026d29aff795db/3:4/w_2808,h_3744,c_limit/GettyImages-484308979.jpg`,
+      imageBox1: `https://images.medicinenet.com/images/article/main_image/what-are-the-3-types-of-tears.jpg`,
+      imageBox3: `https://www.nicepng.com/png/full/13-138625_paper-tearing-cartoon-png.png`,
+      imageBox4: `https://media.istockphoto.com/id/170618066/photo/man-crying.jpg?s=612x612&w=0&k=20&c=VJoy-e2h3oJQqrTLiPg7ogrnVr2tADp6cneo8ACoZUA=`,
+      ansDiv: '',
+      randomAlphabets: 'AYYGEBTYNILR',
+    alphabetDiv: '',
+      answer: 'TEAR',
+     },
+     {
+      imageBox1: `https://cdn.vanguardngr.com/wp-content/uploads/2012/12/Lagbaja-200.jpg`,
+      imageBox2: `https://www.liveabout.com/thmb/DNPhPFtAm2PDIFbcexbdNY2XG-g=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/pop-music-57bce3863df78c87634ea806.jpg`,
+      imageBox3: `https://dailypost.ng/wp-content/uploads/2022/01/portable-1200x1205.jpg`,
+      imageBox4: `https://data.mothership.tools/mothershiptools-ukprod/wp-content/uploads/2021/11/Tems-Square.jpg`,
+      ansDiv: '',
+      randomAlphabets: 'AUYSEBCYNILM',
+    alphabetDiv: '',
+      answer: 'MUSIC',
+     },
+     {
+      imageBox1: `https://upload.wikimedia.org/wikipedia/commons/c/ca/Nicole_Forrester.JPG`,
+      imageBox2: `https://papreplive.com/wp-content/uploads/2021/05/DLN-L-Union-0514-13.jpg`,
+      imageBox3: `https://media.newyorker.com/photos/59834bf5b13fee6a82121c7e/master/w_2560%2Cc_limit/Berthea_Usain-Bolts-Successor.jpg`,
+      imageBox4: `https://www.si.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTg5NTAyNjI5MTc1MzcxMDE0/usatsi_17382888-1.jpg`,
+      ansDiv: '',
+      randomAlphabets: 'WTFSUPNMOQNR',
+    alphabetDiv: '',
+      answer: 'SPORT',
+     },
+     {
+      imageBox1: `https://www.allaboutbirds.org/news/wp-content/uploads/2020/04/RBGull-Vyn-FI-1280x720.jpg`,
+      imageBox2: `https://pestcontrol-uc1.azureedge.net/-/media/Images/Project/RaidSite/Article-Details-Page/Articles/8-Facts-About-Flies/8-Facts-About-Flies-Pinterest.jpg?la=en-US`,
+      imageBox3: `https://gray-wmctv-prod.cdn.arcpublishing.com/resizer/7D7dKUjwlxnA24CB_Jr80I6t0L0=/1200x1200/smart/filters:quality(85)/cloudfront-us-east-1.images.arcpublishing.com/gray/5V62QS6ZHNF6DBA7656F3RS4UY.jpg`,
+      imageBox4: `https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Mosquito_2007-2.jpg/1200px-Mosquito_2007-2.jpg`,
+      ansDiv: '',
+      randomAlphabets: 'WQFOUYLFZXYL',
+    alphabetDiv: '',
+      answer: 'FLY',
+     },
+     {
+      imageBox3: `https://www-konga-com-res.cloudinary.com/w_auto,f_auto,fl_lossy,dpr_auto,q_auto/media/catalog/product/M/U/119643_1530581024.jpg`,
+      imageBox2: `https://media.istockphoto.com/id/492201907/photo/many-tools.jpg?s=612x612&w=0&k=20&c=zCuvI6bjhmpsGkbvKK6vBI6Zovv_bRgIrDNSAC3x3tM=`,
+      imageBox1: `https://media.istockphoto.com/id/476392316/photo/kitchen-utensil.jpg?s=612x612&w=0&k=20&c=Mb2d_7WGJP6N5Jb__soAl74ZUhFwH47iSdJtXeoW47o=`,
+      imageBox4: `https://d7hftxdivxxvm.cloudfront.net/?quality=80&resize_to=width&src=https%3A%2F%2Fartsy-media-uploads.s3.amazonaws.com%2FZ0xYztUZWUscXt08aAy5rw%252FCT08A%2Bok-Portraits.jpg&width=910`,
+      ansDiv: '',
+      randomAlphabets: 'SDHOQOLKSGTB',
+    alphabetDiv: '',
+      answer: 'TOOLS',
+     },
+     {
+      imageBox1: `https://nationaltoday.com/wp-content/uploads/2021/07/shutterstock_1518533924-min.jpg`,
+      imageBox2: `https://res.cloudinary.com/hz3gmuqw6/image/upload/c_fill,q_60,w_750/learn-to-cook-like-a-chef-phpmHibi5`,
+      imageBox3: `https://esmmweighless.com/wp-content/uploads/2019/12/Carolyn-Cooking-Blog.jpg`,
+      imageBox4: `https://www.thedailymeal.com/img/gallery/basic-cooking-methods-you-need-to-know/Basic-Cooking-Methods.jpg`,
+      ansDiv: '',
+      randomAlphabets: 'GHOKBNMKLGOC',
+    alphabetDiv: '',
+      answer: 'COOK',
+     },
+  ]
+  
+  let body = document.querySelector('body');
+  let endPage = document.querySelector('.done-page');
+  let quizBody = document.getElementById('quiz-body');
+  let answers = document.getElementById('answers');
+  let scores = document.querySelector('.scores');
+  let answerCount = document.querySelector('.answer-count');
+  let partition = document.querySelectorAll('.partition');
+  let picked = document.getElementById("picked-alphabets");
+  let clearTiles = document.querySelector(".clear-tiles");
+  let showHint = document.getElementById("show-hint");
+  let hintBtn = document.getElementById("hint");
+  let alphabets = document.getElementById('alphabets');
+  let alphabetOptions = document.getElementById('alphabet-options');
+  const coinSound = document.getElementById('coin-sound');
+
+  
+  let score = 0
+  let currentGame = 0
+  
+  endPage.classList.add('hide')
+  quizBody.classList.remove('hide');
+  
+  loadGame()
+  
+  function loadGame(){
+  let randomData = function () {
+    return game [Math.floor(Math.random()*game.length )]
+  }
+
+  let image1 = document.getElementById('image-1');
+  let image2 = document.getElementById('image-2');
+  let image3 = document.getElementById('image-3');
+  let image4 = document.getElementById('image-4');
+  
+  let randomGameData = randomData()
+
+  image1.src = randomGameData.imageBox1;
+  image2.src = randomGameData.imageBox2;
+  image3.src = randomGameData.imageBox3;
+  image4.src = randomGameData.imageBox4;
+
+
+  scores.innerText = score
+
+  currentQuestion = randomGameData.question;
+  currentAnswer = randomGameData.answer;
+
+
+  picked.innerHTML = randomGameData.ansDiv;
+  alphabets.innerHTML = randomGameData.alphabetDiv;
+
+  createAnswerBox();
+
+  function createAnswerBox() {
+    for (let i = 0; i < currentAnswer.length; i++) {
+      const answerBox = document.createElement('div');
+      answerBox.className = 'picked-input';
+      picked.append(answerBox)
+      }
+   }  
+
+   let letters = randomGameData.randomAlphabets;
+   let letterArray = Array.from(letters)
+
+   result = []
+           
+      loadAlphabets()
+      function loadAlphabets() {
+        for (let i = 0; i < letterArray.length; i++) {
+            let partition = document.createElement('div');
+            partition.className = 'partition';
+            alphabets.append(partition);
+            const button = document.createElement('div');
+            button.className = 'alphabet-option';
+            button.append(letterArray[i]);
+            partition.append(button);
+
+    partition.addEventListener('click', getAlphabets)
+    function getAlphabets() {
+     let chosenAlphabet = this.innerText;
+     result.push(chosenAlphabet)
+     let clickedIndex = letterArray.indexOf(chosenAlphabet);
+     letterArray.splice(clickedIndex, 1)
+      button.remove(chosenAlphabet)
+
+      partition.removeEventListener('click', getAlphabets, false)
+        
+  
+
+      for (let i = 0; i < result.length; i++) {
+        picked.childNodes[i].innerText = result[i]
+
+            
+
+
+        if(picked.textContent.length === currentAnswer.length){
+          
+          if (picked.textContent === currentAnswer) {
+            picked.childNodes.forEach(function(node) {
+              node.style.color = "green";
+              node.style.boxShadow ='green 0 0 5px 2px';
+
+            });
+            coinSound.play();
+              setTimeout(function() {
+                picked.innerText = '';
+                loadGame()
+              }, 500);
+            score++
+            currentGame++
+          }else{
+            picked.childNodes.forEach(function(node) {
+              node.style.color = "red";
+              node.style.boxShadow ='red 0 0 5px 2px';
+            });
+            letterArray.push(...result)
+            
+            setTimeout(function() {
+            result = []
+            picked.innerText = '';
+            alphabets.innerHTML = '';
+            createAnswerBox();
+            loadAlphabets();
+
+              }, 500);
+          }
+
+
+        }
+        }
+
+        if (currentGame >= 5) {
+          quizBody.classList.add('hide')
+          endPage.classList.remove('hide')
+          endPage.innerHTML = 
+          `
+          <h2>You answered ${score} out of ${5} questions correctly. <br> Will you like to play again?</h2>
+          <button onclick="location.reload()">Play Again</button>
+          `
+        }
+
+
+ }
+    }
+  }
+
+
+
+    }
+
+
+
+      
+
+
+  
+
+
+    
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+   
+
